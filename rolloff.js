d2ba941f977421fb75e84842660125a7389c9a9c
Derrick Tao <dstao3489@gmail.com>
2013-11-04 02:36:29 -0800

    feature parity with angularjs
    removed all listeners, ui service, update html

--- rolloff.js
+++ rolloff.js
@@ -1,7 +1,10 @@
 var rollOffApp = angular.module('rollOffApp', ['ui']).
-    service('$game', Game);
+    service('$game', Game).
+    service('$ui', UI);
 
-var UI = {
+function UI() {}
+
+UI.prototype = {
     addRoundHeader: function(n) {
         $('table.table thead tr').append('<th>' + (n + 1) + '</th>');
     },
@@ -34,7 +37,6 @@ var UI = {
     highlightPlayer: function(player) {
         $('tbody tr').removeClass('active');
         $('tbody tr[data-player="' + player.getId() + '"]').addClass('active');
-        $('#notification .panel-body').html("Add score for " + player.getName());
     },
     hideFooterHeader: function() {
         $('#header').hide();
@@ -45,96 +47,75 @@ var UI = {
     },
 }
 
-function AddPlayerCtrl($scope, $game) {
+function InputCtrl($scope, $game, $ui) {
+
+    $scope.notificationText = '';
+
     $scope.addPlayer = function() {
         var name = $scope.playerName;
         if (name != undefined && name != '') { // TOOD: add proper validation via 'required'
             var player = new Player(name);
             $game.addPlayer(player);
             $scope.playerName = '';
-            UI.addPlayer(player);
+            $ui.addPlayer(player);
+            $ui.scrollToElement($('#formInput'));
         }
-        // if($game.players.length > 1) {
-        //     $('#startGame').removeAttr('disabled');
-        // }
     }
-}
 
-function initializeListenersFor(game) {
-    $('#formInput input').focus(function(e) {
-        UI.scrollToElement($(this));
-    })
-    $('#startGame').click(function() { handlers.startGame(game)});
-	$('#formAddScore button').click( function() { 
-        var score = $(this).html();
-        switch(score) {
-            case "-": score = 0; break;
-            case "X": score = 10; break;
-            default: score = parseInt(score);
-        }
-        handlers.addScore(game, score);
-    });
-    $('#restartGame').click(function() { handlers.restartGame(game)});
-}
-
-var handlers = {
-    addPlayer: function(game) {
-        var name = $('#formInput input').val();
-        if (name != undefined && name != '') {
-            var player = new Player(name);
-            UI.addPlayer(player);
-            game.addPlayer(player);
-            $('#formInput input').val('');
-        }
-        if(game.players.length > 1) {
-            $('#startGame').removeAttr('disabled');
-        }
-    },
-    startGame: function(game) {
-		if (game.players.length > 1) {		
-			game.start();
-			UI.addRoundHeader(game.round);
-            var player = game.getCurrentPlayer();
-			$('#formInput').hide();
-			$('#startGame').hide();
-			$('#formAddScore').show();
-            $('#notification').show();
-            UI.highlightPlayer(player);
-            UI.hideFooterHeader();
+    $scope.startGame = function() {
+        if ($game.players.length > 1) {      
+            $game.start();
+            $ui.addRoundHeader($game.round);
+            var player = $game.getCurrentPlayer();
+            $scope.notificationText = "Add score for " + player.getName();
+            $ui.highlightPlayer(player);
+            $ui.hideFooterHeader();
+            $ui.scrollToElement($('table'));
             $('#wrap').css('padding', 5);
-            UI.scrollToElement($('table'));
-		}
-    },
-	restartGame: function(game) {
-        $('#restartGame').hide();
-		UI.removeAllScores();
-        handlers.startGame(game);
-	},
-	addScore: function(game, score) {
-        if (score != undefined && score >= 0 && score <= 10) {
-            var player = game.getCurrentPlayer();
-			var losers = game.addScore(player, score);
-			UI.addScore(player, score);
-            UI.scrollToElement($('tr[data-player="' + player.getId() + '"]'), "fast");
-            UI.handleLosersUI(losers);
+        } 
+    }
 
-			if(game.isOver()) {
-				$('#formAddScore').hide();
-				$('#restartGame').show();
-                $('#notification .panel-body').html(game.restartedPlayerOrder[0].getName() + " is the Winner!").effect('highlight');
-			} else {
-                player = game.getCurrentPlayer();
-                UI.highlightPlayer(player);
-                if(game.cur == 0) {
-                    UI.addRoundHeader(game.round);
+    $scope.canStartGame = function() {
+        return $game.hasEnoughPlayers() && ! $game.isOver();
+    }
+
+    $scope.isGameOver = function() {
+        return $game.isOver();
+    }
+
+    $scope.hasGameStarted = function() {
+        return $game.hasGameStarted();
+    }
+
+    $scope.addScore = function(score) {
+        if (score == 'X') score = 10;
+        if (score != undefined && score >= 0 && score <= 10) {
+            var player = $game.getCurrentPlayer();
+            var losers = $game.addScore(player, score);
+            $ui.addScore(player, score);
+            $ui.scrollToElement($('tr[data-player="' + player.getId() + '"]'), "fast");
+            $ui.handleLosersUI(losers);
+
+            if($game.isOver()) {
+                $scope.notificationText = $game.getWinner().getName() + " is the Winner!";
+            } else {
+                player = $game.getCurrentPlayer();
+                $ui.highlightPlayer(player);
+                $scope.notificationText = "Add score for " + player.getName();
+                if($game.isNewRound()) {
+                    $ui.addRoundHeader($game.round);
                 }
             }
         }
-	}
+    }
+
+    $scope.restartGame = function() {
+        $ui.removeAllScores();
+        $scope.startGame();
+    }
+
 }
 
 $(document).ready(function() {
-    // Game.game = new Game();
-    // initializeListenersFor(Game.game);
-    document.game = angular.element(document).injector().get('$game');
+    // Game.game = angular.element(document).injector().get('$game');
 });
